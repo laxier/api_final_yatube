d91d5752f7f93f787708340822e10c990062e022
Кирилл Т <hash0xcb8c@yandex.ru>
2025-01-24 01:46:41 +0400

    create models for api

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,47 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-
-
 from posts.models import Comment, Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    """
+    Serialize and deserialize Post instances.
+
+    Converts Post model instances into JSON format and vice versa.
+    Includes fields such as author, text, pub_date, and image.
+    """
+
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
+        """
+        Meta options for PostSerializer.
+
+        Specifies the model and fields to include in serialization.
+        """
+
         fields = '__all__'
         model = Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serialize and deserialize Comment instances.
+
+    Converts Comment model instances into JSON format and vice versa.
+    Includes fields such as author, post, text, and created date.
+    """
+
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
     )
 
     class Meta:
+        """
+        Meta options for CommentSerializer.
+
+        Specifies the model and fields to include in serialization.
+        """
+
         fields = '__all__'
         model = Comment
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -12,3 +12,17 @@ urlpatterns = [
     ),
 ]
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+urlpatterns += [
+    path('api/v1/jwt/create/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/v1/jwt/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('api/v1/jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
+
